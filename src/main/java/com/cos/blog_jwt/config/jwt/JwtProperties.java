0beaca187e87b26bba3fb3d481e733ea70a56683
qwq140 <qwq140@gmail.com>
2021-10-20 08:56:18 +0900

    11. jwt 토큰 유효시간 1시간으로 늘림

--- src/main/java/com/cos/blog_jwt/config/jwt/JwtProperties.java
+++ src/main/java/com/cos/blog_jwt/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.cos.blog_jwt.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "JAP";
-    int EXPIATION_TIME = 60000*10; // 10분
+    int EXPIATION_TIME = 60000*60; // 1시간
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
